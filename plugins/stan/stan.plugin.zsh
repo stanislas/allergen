803d56ba56178394d4532abc487812eb4b4e6d07
Stanislas Nanchen <stanislas.nanchen@ergon.ch>
2015-05-09 18:52:59 +0200

    Update stan plugin: move ergon stuff in own ergon hosted repo.

--- plugins/stan/stan.plugin.zsh
+++ plugins/stan/stan.plugin.zsh
@@ -1,64 +1,73 @@
-MYSERVER=paganini.ergon.ch
-for c in tel show_login viaz_activity viaz_bill viaz_item_export viaz_show_bad zpz zaz asknis viaz
-do
-    CMD="alias $c=\"ssh -q -t $MYSERVER $c\""
-    eval $CMD
-done
 
-function viaz_add {
-	ssh -q -t paganini.ergon.ch viaz_add $1 $2 $3 $4 \'$5\'
+export M2_REPO=~/.m2/repository
+export ANT_OPTS="-Xmx2048m -Xms2048m -Djava.awt.headless=true"
+
+export JAVA_HOME=$(/usr/libexec/java_home)
+setjdk() {
+  export JAVA_HOME=$(/usr/libexec/java_home -v $1)
 }
 
-function copy-and-tag-schema {
-	ant -Dschema.tag=${1} copy-and-tag-schema; hg commit -A -m "copy-and-tag-schema ${1}"
-}
+export PATH=~/Library/soft/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin
 
-function move-and-tag-migration {
-	ant -Dfrom.version=${1} -Dto.version=${2} move-and-tag-migration; hg commit -A -m "move-and-tag-migration from ${1} to ${2}"
-}
+export GOROOT=/usr/local/opt/go/libexec
+export GOPATH=$HOME/go
+export PATH=$PATH:$GOPATH/bin
 
-function archive-schema {
-	ant -Dschema.tag=${1} archive-schema; hg commit -A -m "archive schema ${1}";
-}
+export LC_ALL=en_US.UTF-8  
+export LANG=en_US.UTF-8
 
-function archive-migration {
-	ant -Dfrom.version=${1} -Dto.version=${2} archive-migration; hg commit -A -m "archive migration ${1} to ${2}";
-}
+### Gradle
 
-function virgo-pid {
-	ps -ef | grep virgo | grep -v grep | grep java | awk '{print $2}';
-}
+alias gcd='./gradlew --rerun-tasks clean assemble'
+alias gcdt='./gradlew --rerun-tasks clean assemble test'
 
-function viaz-start {
-	/Users/nanchen/Library/soft/viaz-tomcat/bin/startup.sh
-}
+#envfile="$HOME/.gnupg/gpg-agent.env"
+#if [[ -e "$envfile" ]] && kill -0 $(grep GPG_AGENT_INFO "$envfile" | cut -d: -f 2) 2>/dev/null; then
+#    eval "$(cat "$envfile")"
+#else
+#    eval "$(gpg-agent --daemon --enable-ssh-support --write-env-file "$envfile")"
+#fi
+#export GPG_AGENT_INFO  # the env file does not contain the export statement
+# export SSH_AUTH_SOCK   # enable gpg-agent for ssh
+#GPG_TTY=`tty`
+#export GPG_TTY
 
-function viaz-stop {
-	/Users/nanchen/Library/soft/viaz-tomcat/bin/shutdown.sh
-}
 
-function link-training-config {
-	ln -s /home/nanchen/coop/repository/management/testing/server/ch.ergon.mws.test.training/src/test/resources/testdata/training-config/my-first-config .
-}
+# Completion
+autoload -U compinit
+compinit
 
-function cp-jtt {
-	\rm -fr /home/nanchen/soft/jtt
-	cp -r /usr2/ergon/jtt/jtt/dist /home/nanchen/soft/jtt
-}
+# Colors
+autoload -U colors
+colors
 
-function java7 {
-	export JAVA_HOME=/usr/lib/jvm/java-1.7.0
-	export PATH=$JAVA_HOME/bin:${PATH}
-}
+# Globbing
+setopt extendedglob
 
-function va {
-	viaz_activity $1 all
-}
+# Prompt substitution
+setopt prompt_subst
 
-function vag {
-	viaz_activity $1 all | grep -i $2
-}
+# autocd
+setopt autocd
 
-export JAVA_HOME=/usr/java/default
+# Correction
+#setopt correctall
+
+# Advanced prompt support
+autoload -U promptinit
+promptinit
+
+# autocd
+setopt autocd
+
+# History
+export HISTFILE=~/.zsh_history
+export HISTSIZE=100000
+export SAVEHIST=$HISTSIZE
+#setopt hist_ignore_all_dups
+setopt hist_ignore_space
+
+# Complete aliases
+setopt completealiases
 
 alias hg-pull-merge-push-pull='hg pull && hg merge && hg ci -m "merge" && hg push && hg pull -u'
