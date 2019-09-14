52b2340484e1eacfce596fb1c4c5dd9ef3dcf315
Stanislas Nanchen <stan@hood.ch>
2019-09-14 15:06:37 +0200

    cleanup zsh plugin

--- plugins/stan/stan.plugin.zsh
+++ plugins/stan/stan.plugin.zsh
@@ -1,7 +1,3 @@
-
-export M2_REPO=~/.m2/repository
-export ANT_OPTS="-Xmx2048m -Xms2048m -Djava.awt.headless=true"
-
 setjdk() {
   export JAVA_HOME=$(/usr/libexec/java_home -v $1)
 }
@@ -10,18 +6,7 @@ if [[ `uname` == 'Darwin' ]]; then
     setjdk 1.8
 fi
 
-export PATH=~/Library/soft/bin:~/.local/bin:/usr/local/bin:/usr/local/sbin:~/Library/PackageManager/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin:~/engineering/tools/crsh/target/bin:/Users/stan/Library/soft/context/tex/texmf-osx-64/bin:/usr/texbin:/Users/stan/.cargo/bin
-export PATH=$PATH:/usr/local/opt/go/libexec/bin
-
-export GOPATH=$HOME/engineering/go
-export PATH=$GOPATH/bin:$PATH
-
 #export SBT_OPTS="-XX:+CMSClassUnloadingEnabled -Xmx2048m -XX:+UseConcMarkSweepGC -d64"
-
-export LC_ALL=en_US.UTF-8
-export LANG=en_US.UTF-8
-
-export BOOT_JVM_OPTIONS="-Djava.awt.headless=true"
 
 ### localhost80
 
@@ -51,10 +36,8 @@ alias pgstop="pg_ctl -D /usr/local/var/postgres stop -s -m fast"
 ##tvdxtat
 alias tvdxtat="/Users/stan/Library/soft/tvdxtat/tvdxtat.sh"
 
-##spectralogs
-alias skylogs="envdir ${HOME}/.sky awslogs"
-alias spectradevlogs="envdir ${HOME}/.spectradev awslogs"
-alias libradevfflogs="envdir ${HOME}/.libradevff awslogs"
+##
+alias brewery="brew update && brew upgrade && brew cleanup"
 
 ## GPG
 export LEIN_GPG=gpg
@@ -74,26 +57,10 @@ export LEIN_GPG=gpg
 #export TNS_ADMIN=$DYLD_LIBRARY_PATH
 #export PATH=$PATH:$DYLD_LIBRARY_PATH
 
-#local runit
-export SVDIR=/Users/stan/Library/service
-
-#node
-export PATH="/usr/local/opt/node@6/bin:$PATH"
-
-#bellboy
-alias tunnel-bellboy-qa="ssh -i ~/.ssh/aws-sky.pem -L4406:bef8-cluster.cluster-cz7wjvhcrwgr.eu-west-1.rds.amazonaws.com:3306  ec2-user@ec2-54-77-218-248.eu-west-1.compute.amazonaws.com"
-
 #nix
 #if [ -f /Users/stan/.nix-profile/etc/profile.d/nix.sh ]; then
 #	. /Users/stan/.nix-profile/etc/profile.d/nix.sh
 #fi
-
-# Emacs
-
-export EDITOR="vim"
-alias e="emacsclient -t"
-alias ec="emacsclient -c"
-alias e-kill="emacsclient -e '(kill-emacs)'"
 
 # Completion
 autoload -U compinit
@@ -137,33 +104,3 @@ if [ -s ~/.secrets ]
 then
 	source ~/.secrets
 fi
-
-if [ -s ~/.opam/opam-init/init.zsh ]
-then
-	 . ~/.opam/opam-init/init.zsh > /dev/null 2> /dev/null || true
-fi
-
-# rbenv
-if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
-
-# leda
-alias leda-repl="docker exec -it leda-dev2 /root/work/leda/tools/repl-dev-docker.sh"
-alias leda-bash="docker exec -it leda-dev2 /bin/bash"
-
-# neutron core
-alias nc-repl="docker exec -it neutron-core-dev /root/work/neutron-core/tools/repl-dev-docker.sh"
-alias nc-bash="docker exec -it neutron-core-dev /bin/bash"
-
-# monolith 
-alias mono-repl="docker exec -it monolith-dev /root/work/monolith/tools/repl-dev-docker.sh"
-alias mono-bash="docker exec -it monolith-dev /bin/bash"
-
-# timeflow 
-alias tf-repl="docker exec -it timeflow-dev /root/work/timeflow/tools/repl-dev-docker.sh"
-alias tf-bash="docker exec -it timeflow-dev /bin/bash"
-
-# python 3
-export PATH="/Library/Frameworks/Python.framework/Versions/3.6/bin:${PATH}"
-
-# pipenv
-export PIPENV_VENV_IN_PROJECT=1
