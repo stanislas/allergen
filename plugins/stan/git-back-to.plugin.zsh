4c4e687ea3cc4135580433e8f65cd0ef3ce20da5
Stanislas Nanchen <stan@codesmith.ch>
2023-10-26 12:48:25 +0200

    try to autocomplete for git back-to
    Signed-off-by: Stanislas Nanchen <stan@codesmith.ch>

---
+++ plugins/stan/git-back-to.plugin.zsh
@@ -0,0 +1,23 @@
+# Filename: _mycommand
+#compdef git-back-to
+
+_git-back-to() {
+ local branches
+  branches=($(git for-each-ref --format='%(refname:short)' refs/heads/))
+
+  _arguments \
+    '1:branches:->branches' \
+    '*:: :->args'
+
+  case $state in
+    (branches)
+      _describe 'branches' branches && ret=0
+      ;;
+    (args)
+      # Your custom arguments and options for the mycommand subcommand
+      # ...
+      ;;
+  esac
+}
+
+_git-back-to "$@"
