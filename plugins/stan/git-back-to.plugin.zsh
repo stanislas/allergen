9cdbc5b38ec648377ed5cd04cdc1b3fc7a778215
Stanislas Nanchen <stan@codesmith.ch>
2023-10-26 12:50:18 +0200

    next attempt
    Signed-off-by: Stanislas Nanchen <stan@codesmith.ch>

--- plugins/stan/git-back-to.plugin.zsh
+++ plugins/stan/git-back-to.plugin.zsh
@@ -2,22 +2,9 @@
 #compdef git-back-to
 
 _git-back-to() {
- local branches
+  local branches
   branches=($(git for-each-ref --format='%(refname:short)' refs/heads/))
-
-  _arguments \
-    '1:branches:->branches' \
-    '*:: :->args'
-
-  case $state in
-    (branches)
-      _describe 'branches' branches && ret=0
-      ;;
-    (args)
-      # Your custom arguments and options for the mycommand subcommand
-      # ...
-      ;;
-  esac
+  _describe 'branches' branches && ret=0
 }
 
 _git-back-to "$@"
